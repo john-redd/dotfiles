8721947dc78b6d13bcb8e60bf7e6bb8bd5732d78
John Redd <john.redd@endevvr.io>
2022-07-25 20:03:17 +0200

    Fine tune nvim gruvbox theme

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -7,11 +7,12 @@ local colorscheme = "gruvbox"
 vim.o.background = "dark" -- or "light" for light mode
 local gruvbox_status_ok, gruvbox = pcall(require, "gruvbox")
 if not gruvbox_status_ok then
-  return
+	return
 end
 
 gruvbox.setup({
-  contrast = "soft",
+	contrast = "soft",
+	italic = false,
 })
 
 -- Monokai --
@@ -26,5 +27,5 @@ gruvbox.setup({
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end

f6847c05837bcabe069a3cd25f2b65cf01fef119
John Redd <john.redd@endevvr.io>
2022-12-19 16:51:18 -0700

    Add nvim tree

--- .config/nvim/lua/endevvr/packer.lua
+++ .config/nvim/lua/endevvr/packer.lua
@@ -1,80 +1,86 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+vim.cmd.packadd("packer.nvim")
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make", cond = vim.fn.executable("make") == 1 })
 
-  use('folke/tokyonight.nvim')
-  use('ellisonleao/gruvbox.nvim')
-  use('tanvirtin/monokai.nvim')
-  use('folke/tokyonight.nvim')
+  use("folke/tokyonight.nvim")
+  use("ellisonleao/gruvbox.nvim")
+  use("tanvirtin/monokai.nvim")
+  use("folke/tokyonight.nvim")
   use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-          vim.cmd('colorscheme rose-pine')
-      end
+    "rose-pine/neovim",
+    as = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+    end,
   })
   use({
-	  'catppuccin/nvim',
-	  as = 'cappuccin',
-	  run = ':CatppuccinCompile'
+    "catppuccin/nvim",
+    as = "cappuccin",
+    run = ":CatppuccinCompile",
   })
 
-  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("nvim-treesitter/playground")
   use({
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      after = 'nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
   })
 
-
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
+  use("theprimeagen/harpoon")
+  use("mbbill/undotree")
 
   -- Git
-  use('tpope/vim-fugitive')
-  use('lewis6991/gitsigns.nvim')
+  use("tpope/vim-fugitive")
+  use("lewis6991/gitsigns.nvim")
 
-  use('numToStr/Comment.nvim')
+  use("numToStr/Comment.nvim")
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-      {"jose-elias-alvarez/null-ls.nvim"},
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "jose-elias-alvarez/null-ls.nvim" },
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+  })
 
   use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
+  -- use("github/copilot.vim")
   use("moll/vim-bbye")
 
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons", -- optional, for file icons
+    },
+    tag = "nightly", -- optional, updated every week. (see issue #1193)
+  })
 end)
-

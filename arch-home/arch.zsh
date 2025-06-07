e949992bd1c702253a5c36c758abf61c49a5c739
John Redd <john.redd@endevvr.io>
2025-06-06 23:14:40 -0600

    Add mise

--- arch-home/arch.zsh
+++ arch-home/arch.zsh
@@ -1,1 +1,14 @@
 eval "$(starship init zsh)"
+
+append_path () {
+    case ":$PATH:" in
+        *:"$1":*)
+            ;;
+        *)
+            PATH="${PATH:+$PATH:}$1"
+    esac
+}
+
+append_path '/home/john/.local/bin'
+
+eval "$(/home/john/.local/bin/mise activate zsh)"

853d3cdccc57af67d3007a50cdd7d9c3568d4020
John Redd <john.redd@endevvr.io>
2025-06-09 15:49:23 -0600

    Improve vim-tmux-navigator.

---
+++ nvim/.config/nvim/after/plugin/vim-tmux-navigator.lua
@@ -0,0 +1,7 @@
+vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
+
+vim.g.tmux_navigator_disable_when_zoomed = 1
+vim.g.tmux_navigator_no_wrap = 1

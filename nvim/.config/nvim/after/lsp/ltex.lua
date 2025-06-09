280644581ae7aa6365ae6d769c6921fb454996f1
John Redd <john.redd@endevvr.io>
2025-06-09 15:07:06 -0600

    Add html and ltex lsp configs.

---
+++ nvim/.config/nvim/after/lsp/ltex.lua
@@ -0,0 +1,7 @@
+return {
+  settings = {
+    ltex = {
+      enabled = { "bibtex", "gitcommit", "markdown", "org", "tex", "restructuredtext", "rsweave", "latex", "quarto", "rmd", "context", "mail", "plaintext" }
+    }
+  }
+}

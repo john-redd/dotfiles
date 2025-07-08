d7e8cdfc31f35ccf6cc7a981fa4188588e8462c7
John Redd <john.redd@endevvr.io>
2025-07-08 16:37:37 -0600

    Add rust analyzer.

---
+++ nvim/.config/nvim/after/lsp/rust_analyzer.lua
@@ -0,0 +1,11 @@
+return {
+  settings = {
+    ['rust-analyzer'] = {
+      inlayHints = {
+        typeHints = { enable = false },
+        parameterHints = { enable = true },
+        chainingHints = { enable = false },
+      },
+    }
+  }
+}

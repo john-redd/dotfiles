341253d3a8efe808a0cdec0cb88cb88e2e8771d8
John Redd <john.redd@endevvr.io>
2025-07-22 16:44:04 -0600

    Add zsh syntax highlight and autocomplete to arch.

--- arch-home/arch.zsh
+++ arch-home/arch.zsh
@@ -32,5 +32,8 @@ alias n=nvim
 
 eval "$(atuin init zsh)"
 
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
 # Needs to be run to make qutebrowser the default browser for xdg
 # BROWSER=""; xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop

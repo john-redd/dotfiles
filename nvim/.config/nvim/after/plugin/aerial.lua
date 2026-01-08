c4a4e2c7cc5ebfd79f5b618c1812feb370f9470d
John Redd <john.redd@endevvr.io>
2026-01-08 15:02:27 -0700

    Aerial changes.

--- nvim/.config/nvim/after/plugin/aerial.lua
+++ nvim/.config/nvim/after/plugin/aerial.lua
@@ -3,6 +3,19 @@ local aerial_ok, aerial = pcall(require, "aerial")
 if aerial_ok then
   aerial.setup({
     backends = { "lsp", "treesitter", "markdown", "asciidoc", "man" },
+    filter_kind = {
+      ["_"] = {
+        "Class",
+        "Constructor",
+        "Enum",
+        "Function",
+        "Interface",
+        "Module",
+        "Method",
+        "Struct",
+      },
+      markdown = false
+    },
   })
   vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 end

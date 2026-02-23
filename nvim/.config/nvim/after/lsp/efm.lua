109fb0f1d82c431b722884914513691cbc882a8b
John Redd <john.redd@endevvr.io>
2026-02-23 15:30:50 -0700

    Fix efm bug.

--- nvim/.config/nvim/after/lsp/efm.lua
+++ nvim/.config/nvim/after/lsp/efm.lua
@@ -1,58 +1,60 @@
-local eslint = require('efmls-configs.linters.eslint_d')
-local prettier = require('efmls-configs.formatters.prettier_d')
-local cspell = require('efmls-configs.linters.cspell')
+local eslint = require("efmls-configs.linters.eslint_d")
+local prettier = require("efmls-configs.formatters.prettier_d")
+local cspell = require("efmls-configs.linters.cspell")
 
-local languages = require('efmls-configs.defaults').languages()
-languages = vim.tbl_extend('force', languages, {
-  typescript = { eslint, prettier },
-  typescriptreact = { eslint, prettier },
-  javascript = { eslint, prettier },
-  javascriptreact = { eslint, prettier },
-  html = { prettier },
-  css = { prettier },
+local languages = require("efmls-configs.defaults").languages()
+languages = vim.tbl_extend("force", languages, {
+	typescript = { eslint, prettier },
+	typescriptreact = { eslint, prettier },
+	javascript = { eslint, prettier },
+	javascriptreact = { eslint, prettier },
+	html = { prettier },
+	css = { prettier },
 })
 
 local cspell_languages = {
-  'javascript',
-  'javascriptreact',
-  'typescript',
-  'typescriptreact',
-  'css',
-  'html',
-  'json',
-  'jsonc',
-  'yaml',
-  'markdown',
+	"javascript",
+	"javascriptreact",
+	"typescript",
+	"typescriptreact",
+	"css",
+	"html",
+	"json",
+	"jsonc",
+	"yaml",
+	"markdown",
 }
 
 for _, pl in pairs(cspell_languages) do
-  languages[pl] = { cspell }
+  if languages[pl] ~= nil then
+    languages[pl] = vim.tbl_extend("force", languages[pl], { cspell })
+  end
 end
 
 local efmls_config = {
-  filetypes = vim.tbl_keys(languages),
-  settings = {
-    rootMarkers = { '.git/' },
-    languages = languages,
-  },
-  init_options = {
-    documentFormatting = true,
-    documentRangeFormatting = true,
-  },
+	filetypes = vim.tbl_keys(languages),
+	settings = {
+		rootMarkers = { ".git/" },
+		languages = languages,
+	},
+	init_options = {
+		documentFormatting = true,
+		documentRangeFormatting = true,
+	},
 }
 
-local lsp_fmt_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
-vim.api.nvim_create_autocmd('BufWritePost', {
-  group = lsp_fmt_group,
-  callback = function(ev)
-    local efm = vim.lsp.get_clients({ name = 'efm', bufnr = ev.buf })
+local lsp_fmt_group = vim.api.nvim_create_augroup("LspFormattingGroup", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = lsp_fmt_group,
+	callback = function(ev)
+		local efm = vim.lsp.get_clients({ name = "efm", bufnr = ev.buf })
 
-    if vim.tbl_isempty(efm) then
-      return
-    end
+		if vim.tbl_isempty(efm) then
+			return
+		end
 
-    vim.lsp.buf.format({ name = 'efm' })
-  end,
+		vim.lsp.buf.format({ name = "efm" })
+	end,
 })
 
 -- If using nvim >= 0.11 then use the following

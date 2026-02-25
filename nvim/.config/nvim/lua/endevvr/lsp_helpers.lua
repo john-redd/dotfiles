e1133c30a0b3bef573f587c7b65a6162509468e3
John Redd <john.redd@endevvr.io>
2026-02-25 14:26:30 -0700

    reformat and add way to disable gopls fmt.

--- nvim/.config/nvim/lua/endevvr/lsp_helpers.lua
+++ nvim/.config/nvim/lua/endevvr/lsp_helpers.lua
@@ -1,62 +1,72 @@
-local function has_value(haystack, needle)
-  for _index, value in ipairs(haystack) do
-    if value == needle then
-      return true
-    end
-  end
-
-  return false
+local function create_set(list)
+	local set = {}
+	for _, l in ipairs(list) do
+		set[l] = true -- Store the value as a key with a truthy value
+	end
+	return set
 end
+
+local disable_formatting_list = { "gopls", "golangci-lint-langserver" }
+local disable_formatting_set = create_set(disable_formatting_list)
 
 local function on_attach(client, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
 
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-  end
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
 
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-  nmap("<leader>vd", vim.diagnostic.open_float, "Open Float ?")
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>vd", vim.diagnostic.open_float, "Open Float ?")
 
-  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+	nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-  -- See `:help K` for why this keymap
-  -- See after/ftplugin/rust.lua for where these are configured.
-  if vim.bo[bufnr].filetype ~= "rust" then
-    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  end
+	-- See `:help K` for why this keymap
+	-- See after/ftplugin/rust.lua for where these are configured.
+	if vim.bo[bufnr].filetype ~= "rust" then
+		nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+		nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	end
 
-  -- Lesser used LSP functionality
-  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
-  nmap("gl", vim.diagnostic.open_float, "Open Diagnostic")
+	if disable_formatting_set[client.config.cmd[1]] ~= nil then
+		client.server_capabilities.documentFormattingProvider = false
+	end
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    if vim.lsp.buf.format then
-      vim.lsp.buf.format()
-    elseif vim.lsp.buf.formatting then
-      vim.lsp.buf.formatting()
-    end
-  end, { desc = "Format current buffer with LSP" })
+	-- Lesser used LSP functionality
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
+	nmap("gl", vim.diagnostic.open_float, "Open Diagnostic")
 
-  vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>", { desc = "Run :Format" })
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		if disable_formatting_set[client.name] ~= nil then
+			return
+		end
+
+		if vim.lsp.buf.format then
+			vim.lsp.buf.format()
+		elseif vim.lsp.buf.formatting then
+			vim.lsp.buf.formatting()
+		end
+	end, { desc = "Format current buffer with LSP" })
+
+	vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>", { desc = "Run :Format" })
 end
+
 local Module = {
-  on_attach = on_attach,
+	on_attach = on_attach,
 }
 
 return Module

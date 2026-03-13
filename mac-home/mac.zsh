88efb022161c49f11f15cd239b98d6c2983ae2d2
John Redd <john.redd@endevvr.io>
2026-03-13 14:46:17 -0600

    mac installs

--- mac-home/mac.zsh
+++ mac-home/mac.zsh
@@ -7,3 +7,5 @@ append_path "$HOME/.opencode/bin"
 source /opt/homebrew/Cellar/zsh-syntax-highlighting/0.8.0/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /opt/homebrew/Cellar/zsh-autosuggestions/0.7.1/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+alias sed="gsed"
+

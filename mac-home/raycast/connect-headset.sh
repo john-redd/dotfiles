2627c83c77465cf30755554db2392eff12e52673
John Redd <john.redd@endevvr.io>
2026-03-27 13:02:22 -0600

    add bluetooth connect script.

---
+++ mac-home/raycast/connect-headset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title connect-headset
+# @raycast.mode compact
+
+BluetoothConnector --connect EC-66-D1-C0-12-66 --notify

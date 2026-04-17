7d0f3d648ec319f42be623b7ff1b993a88080bb2
John Redd <john.redd@endevvr.io>
2026-04-16 20:06:04 -0600

    install sqls and bashls.

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -9,6 +9,9 @@ require("mason-lspconfig").setup({
     "docker_compose_language_service",
     "dockerls",
     "html",
+    "sqls",
+    "bashls",
+    -- "codespell",
   },
   automatic_enable = {
     exclude = {
